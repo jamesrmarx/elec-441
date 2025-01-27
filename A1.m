349f6909b3021db320f63fd37f0033b42f04cf3e
James Marx <jamesrmarx@gmail.com>
2025-01-26 17:07:53 -0800

    added discrete time simulation and subplots

--- A1.m
+++ A1.m
@@ -19,6 +19,9 @@ xo = [1;
 % Set-up time step and time vector
 dT = 0.01;
 t = 0:dT:10;
+T = 0.1;
+
+kt = 0:T:10;
 
 % Input vector - sin(t); 0 <= t <= pi
 u = zeros(size(t));
@@ -27,6 +30,39 @@ u(t <= pi) = sin(t(t <= pi));
 
 sys = ss(A,B,C,D);
 
-lsim(sys,u,t,xo);
+sim_result = lsim(sys,u,t,xo);
 
+sysd = c2d(sys,T);
+
+x_k = zeros(2, length(kt));
+% Set initial conditions
+x_k(:,1) = [1;1];
+u_k = zeros(size(kt));
+u_k(kt <= pi) = sin(kt(kt <= pi));
+
+for k = 1:length(kt)-1
+    x_k(:,k+1) = sysd.A*x_k(:,k) + sysd.B*u_k(k);
+end
+
+figure
+
+subplot(4, 1, 1)
+plot(t, sim_result(:,1))
+hold on
+plot(t, u)
+
+subplot(4, 1, 2)
+plot(t, sim_result(:,2))
+hold on
+plot(t, u)
+
+subplot(4, 1, 3)
+plot(kt, x_k(1,:))
+hold on
+plot(kt, u_k)
+
+subplot(4, 1, 4)
+plot(kt, x_k(2,:))
+hold on
+plot(kt, u_k)
 
